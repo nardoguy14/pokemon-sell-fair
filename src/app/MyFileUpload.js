e4779af0d37de705db04a340d243cbbba9846264
Nardo Arevalo <nardoarevalo@Nardos-MacBook-Air.local>
2023-05-11 20:52:55 -0700

    adding quantities and default percentage

--- src/app/MyFileUpload.js
+++ src/app/MyFileUpload.js
@@ -9,21 +9,41 @@ import React, { Component } from 'react';
 export default class SellTable extends Component {
     constructor(props) {
         super(props)
+        const DEFAULT_PERCENTAGE = 0.7
 
         var sellPrices = Array(this.props.data.length).fill(0)
         const nextSellPrices= sellPrices.map((c, i) => {
-            return 0.7 * this.determinePrice(this.props.data[i]);
+            return DEFAULT_PERCENTAGE* this.determinePrice(this.props.data[i]) * this.props.data[i].quantity
         });
+
+
         this.state = {
             data: this.props.data,
             percentages: Array(this.props.data.length).fill(0.7),
-            sellPrices: nextSellPrices
+            sellPrices: nextSellPrices,
+            defaultPercentage: DEFAULT_PERCENTAGE
         };
 
     }
 
     componentDidMount(){
 
+    }
+
+    changeDefaultPercentage(e) {
+        var percentage = e.target.value
+        var newPercentages = this.state.percentages.map((c, i) => {
+            return percentage
+        });
+        const nextSellPrices= this.state.sellPrices.map((c, i) => {
+            return newPercentages[i] * this.determinePrice(this.state.data[i]) * this.state.data[i].quantity
+
+        });
+        this.setState({
+            percentages: newPercentages,
+            defaultPercentage: percentage,
+            sellPrices: nextSellPrices
+        })
     }
 
     changeSellPercentage(row, e) {
@@ -37,7 +57,7 @@ export default class SellTable extends Component {
 
         const nextSellPrices= this.state.sellPrices.map((c, i) => {
             if (i === row) {
-                return nextPercentages[row] * this.determinePrice(this.state.data[row]);
+                return nextPercentages[row] * this.determinePrice(this.state.data[row]) * this.state.data[i].quantity
             } else {
                 return c;
             }
@@ -91,6 +111,9 @@ export default class SellTable extends Component {
                     <td>
                         <a href={cardUrl} target="_blank">{card.name}</a>
                     </td>
+                    <td>
+                        {card.quantity}
+                    </td>
                     <td>${price}</td>
                     <td>
                         <input id={"percentage-"+index}
@@ -141,8 +164,18 @@ export default class SellTable extends Component {
                     <tr>
                         <th></th>
                         <th>Name & TCG Link</th>
+                        <th>Quantity</th>
                         <th>Market Price</th>
-                        <th>Sell Percentage</th>
+                        <th>
+                            Sell Percentage
+                            <br/>
+                            Default:
+                            <input
+                                id={"weeeeeee"}
+                                type="input"
+                                value={this.state.defaultPercentage}
+                                onChange={(e) => this.changeDefaultPercentage(e)} />
+                        </th>
                         <th>Sell Price</th>
                     </tr>
                     </thead>
